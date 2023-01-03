44440d9a3bc65a8b057a75da95c1511f25801b58
Mustafa-Hassan2001 <98483655+Mustafa-Hassan2001@users.noreply.github.com>
2023-01-03 23:24:08 +0500

    Add files via upload

---
+++ Solve Me First.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int solveMeFirst(int a, int b){ 
+    return a+b;
+}
+
+int main(){
+    int a,b;
+    cin>>a;
+    cin>>b;
+    cout<<solveMeFirst(a,b);
+
+}
